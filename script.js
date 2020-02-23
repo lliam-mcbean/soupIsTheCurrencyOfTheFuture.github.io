72300794bfa2a75559a86dcc76334286f5f2e55f
Lliam McBean <60054273+soupIsTheCurrencyOfTheFuture@users.noreply.github.com>
2020-02-22 20:50:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let $star = $('.star');
-let $win = $(window);
 
-$win.on('scroll', function() {
-    let top = $win.scrollTop();
-    $star.css('transform', 'rotate(' + top + 'deg)');
+
+window.addEventListener('scroll', function () {
+    let scrolledSlowed = window.scrollY / 8;
+    let scrolled = window.scrollY;
+    let scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    let box1 = document.querySelector('.box-1');
+    let box2 = document.querySelector('.box-2');
+    let box3 = document.querySelector('.box-3');
+    
+    
+    box1.style.transform =  'translateX('+ scrolledSlowed +'vw)';
+    box2.style.transform =  'translateX(-'+ scrolledSlowed +'vw)';
+    box3.style.transform =  'translateY(-'+ scrolledSlowed +'vw)';
+
+    console.log(scrollable);
+    console.log(scrolled);
+
+    if (scrolled > 220) {
+        document.querySelector('.box-4').className = 'box-4-active';
+        document.querySelector('.box-5').className = 'box-5-active';
+        document.querySelector('.box-6').className = 'box-6-active';
+    } else {
+        document.querySelector('.box-4-active').className = 'box-4';
+        document.querySelector('.box-5-active').className = 'box-5';
+        document.querySelector('.box-6-active').className = 'box-6';
+    }
 });
